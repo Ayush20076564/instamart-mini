9df5c595a7361d89f41be5bb04e2e089e39aec9f
Ayush20076564 <20076564@mydbs.ie>
2025-11-24 12:58:52 +0000

    DB models

--- app.py
+++ app.py
@@ -1,14 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, request, render_template, session
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
 
 app = Flask(__name__)
+app.secret_key = "instamart-secret"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL", "sqlite:///instamart.db")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 CORS(app)
 
+# ---------------- Models ---------------- #
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    role = db.Column(db.String(10), default="user")  # user/admin
 
-@app.route('/login')
-def home():
-    return render_template('login.html')
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    price = db.Column(db.Float)
+    quantity = db.Column(db.Integer)
+    image = db.Column(db.String(200), nullable=True)
 
+class Cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('item.id'))
+    quantity = db.Column(db.Integer, default=1)
 
-if __name__ == '__main__':
-    app.run(debug=True)
